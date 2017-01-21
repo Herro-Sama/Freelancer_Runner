f8cba5d179de8e0aa2634c12dffcbba01e962265
Alex <sandylily66@hotmail.com>
2017-01-21 21:24:51 +0000

    Added other things

---
+++ Assets/Code/Despawn.cs
@@ -0,0 +1,25 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class Despawn : MonoBehaviour {
+    private float deathTime;
+    public float deathDelay = 2;//two seconds delay.
+
+    // Use this for initialization
+    void Start()
+    {
+        deathTime = Time.time + deathDelay;
+    }
+
+    // Update is called once per frame
+
+    void Update()
+    {
+        if (Time.time > deathTime)
+        {
+            deathTime = Time.time + deathDelay;
+            Destroy(gameObject);
+        }
+
+    }
+}
---
+++ Assets/Code/MakeDead.cs
@@ -0,0 +1,33 @@
+﻿using UnityEngine;
+using System.Collections;
+using System;
+
+
+public class MakeDead : MonoBehaviour
+{
+    private float AnimationTime = 1.6f;
+    Animator anim;
+
+    void Start()
+    {
+        anim = GetComponent<Animator>();
+    }
+
+    void Update()
+    {
+        GameObject Text = GameObject.Find("Text");
+        HealthScript targetScript = Text.GetComponent<HealthScript>();
+
+        if (targetScript.Health <= 0)
+        {
+            anim.SetTrigger("PlayerDead");
+            StartCoroutine(DeathScreen());
+        }
+    }
+            IEnumerator DeathScreen()
+            {       
+                yield return new WaitForSeconds(AnimationTime);
+                Application.LoadLevel("GameOver");
+
+             }
+}
