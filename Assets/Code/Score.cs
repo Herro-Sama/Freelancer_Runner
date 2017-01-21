4853470515ec965cc4855415a150c71a0f94209b
Phil-Sparkes <phil.sparkes95@gmail.com>
2017-01-21 19:44:28 +0000

    Add files via upload

---
+++ Assets/Code/Score.cs
@@ -0,0 +1,22 @@
+﻿using UnityEngine;
+using System.Collections;
+using UnityEngine.UI;
+
+public class Score : MonoBehaviour
+
+{
+    Text TextBox;
+    float PlayerScore = 0;
+    // Use this for initialization
+    void Start()
+    {
+        TextBox = gameObject.GetComponent<Text>();
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        PlayerScore += (1 * Time.deltaTime);
+        TextBox.text = "Score = " + (int)PlayerScore;
+    }
+}
